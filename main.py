41033051ac0e264e2d396296630c99b42aedc479
Daniel <anza.vossos@protonamil.com>
2025-01-23 13:11:58 -0800

    .

--- main.py
+++ main.py
@@ -15,28 +15,30 @@ if __name__ == "__main__":
     # installer = WinServer2016Installer()
     # installer.start(installer.handle_installations)
 
-    # server_config = ServerConfig(
-    #     username=os.getenv("SERVER_USERNAME"),
-    #     public_key=os.getenv("SERVER_PUBLIC_KEY")
-    # )
-    #
-    # ssh_manager = WinServer2016ServerManager(server_config)
-    # ssh_manager.handle_installation()
+    server_config = ServerConfig(
+        username=os.getenv("SERVER_USERNAME"),
+        public_key=os.getenv("SERVER_PUBLIC_KEY")
+    )
 
     server_agent_config = AgentConfig(
         address=os.getenv("AGENT_ADDRESS"),
         token=os.getenv("AGENT_TOKEN")
     )
-    #
-    # agent_manager = AgentManager(server_agent_config)
-    # agent_manager.handle_installation()
-
-    # ServiceManager.to_exe()
 
     rdp_config = RDPConfig(
         username=os.getenv("RDP_USERNAME"),
         password=os.getenv("RDP_PASSWORD")
     )
+
+    # ssh_manager = WinServer2016ServerManager(server_config)
+    # ssh_manager.handle_installation()
+
+
+    #
+    # agent_manager = AgentManager(server_agent_config)
+    # agent_manager.handle_installation()
+
+    # ServiceManager.to_exe()
 
     rdp_manager = RDPManager(rdp_config)
     rdp_manager.handle_installation()
