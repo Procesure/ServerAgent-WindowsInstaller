3473bf59db7b73fe0126ab85638d196012c637c6
Daniel <anza.vossos@protonamil.com>
2025-01-23 07:43:46 -0800

    .

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
 from installers.winserver2016 import WinServer2016Installer
 from managers.open_ssh.manager import WinServer2016OpenSSHManager, SSHConfig
+
+load_dotenv()
 
 if __name__ == "__main__":
 
     # installer = WinServer2016Installer()
     # installer.start(installer.handle_installations)
 
-    manager = WinServer2016OpenSSHManager(config=SSHConfig(username="123", public_key="123"))
+    manager = WinServer2016OpenSSHManager(config=SSHConfig(username=os.getenv("SSH_USERNAME"), public_key=os.getenv("SSH_PUBLIC_KEY")))
     manager.handle_installation()
